62cb31d2ed73526bff226a51c29269c8b1fab662
Rishikeshkumar1 <rishikr1702@gmail.com>
2026-01-15 11:46:30 +0530

    Upgrade Java to 21 LTS and update Cucumber version

--- src/test/java/com/example/base/TestBase.java
+++
@@ -1,9 +0,0 @@
-package com.example.base;
-
-import org.openqa.selenium.WebDriver;
-
-public class TestBase {
-    protected static WebDriver driver;
-
-    // In a small sample we leave lifecycle to DriverFactory or test runner.
-}
