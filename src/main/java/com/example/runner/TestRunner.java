e6e6b33810d9a7cb41c65d0c6332a8d55bdbb579
SohamTodkari01 <todkarisoham@gmail.com>
2026-01-15 15:04:57 +0530

    run

---
+++ src/main/java/com/example/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package com.example.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+    features = "src/main/resources/features",
+    glue = {"com.example.steps"},
+    tags = "@test",
+    plugin = {"pretty", "html:target/cucumber-report.html"},
+    monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();}
+}
---
+++ src/main/resources/features/login.feature
@@ -0,0 +1,7 @@
+Feature: Login
+
+  @test
+  Scenario: Valid login
+    Given User navigates to login page
+  	When User enters valid credentials
+    Then User enters valid credentials
